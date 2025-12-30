00d796eceef370d8b5d83fa2c7e3e541a423cead
Dmytro Hopko <gopkodimon@gmail.com>
2025-12-30 15:48:34 +0100

    Refactor landing page: remove old page and implement new design with logo and login button

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,3 +1,23 @@
+import Link from 'next/link';
+import { authRoutes } from '@/config/routes.config';
+import { Button } from '@/components/ui/button';
+import { Logo } from '@/components/custom-icons';
+
 export default function LandingPage() {
-  return <h1 className="text-2xl font-bold">Landing Page</h1>;
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Logo size={64} />
+        <h1 className="text-2xl font-bold text-center">Invoce Hopko</h1>
+      </div>
+      <h2 className="text-xl font-bold text-center">
+        Welcome to the Landing Page
+      </h2>
+      <Link href={authRoutes.signIn}>
+        <Button variant="outline" className="w-[200px]">
+          Login
+        </Button>
+      </Link>
+    </div>
+  );
 }
